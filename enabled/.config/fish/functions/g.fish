35293285749339dc9dc0cc4077c043b9f4391e29
Julien Thewys <julien.thewys@gmail.com>
2017-03-24 12:16:34 +0100

    retab

--- enabled/.config/fish/functions/g.fish
+++ enabled/.config/fish/functions/g.fish
@@ -1,20 +1,21 @@
-#echo -n "example: g 'def fib.*:$' py"
 function g
-	if type -q ag
-		ag $argv
-	else if type -q ack
-		ack $argv
-	else
-		switch (count $argv);
-			case 0
-				echo "usage: g pattern [file extension]"
-			case 1
-				grep -rn --color $argv[1]
-			case 2
-				find . -iname \*.$argv[2] -exec grep -n --color $argv[1] \{\} \+
-			case '*'
-				echo "Too many arguments, should be <3"
-		end
-	end
+    if type -q ag
+        ag $argv
+    else if type -q ack
+        ack $argv
+    else
+        switch (count $argv);
+            case 0
+                echo "usage: g pattern [file extension]"
+                echo
+                echo "example: g 'def fib.*:\$' py"
+            case 1
+                grep -rn --color $argv[1]
+            case 2
+                find . -iname \*.$argv[2] -exec grep -n --color $argv[1] \{\} \+
+            case '*'
+                echo "Too many arguments, should be <3"
+        end
+    end
 end
 
