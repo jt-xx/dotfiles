3a22c2b128ec90cfacd2fd761482466933b7572c
Julien Thewys <julien.thewys@gmail.com>
2017-03-24 12:16:56 +0100

    prompt clarity

--- enabled/.config/fish/functions/fish_prompt.fish
+++ enabled/.config/fish/functions/fish_prompt.fish
@@ -7,27 +7,28 @@ function fish_prompt --description "Write out the prompt"
     set -l color_cwd
     set -l suffix
     switch $USER
-    case root toor
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
-        else
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
             set color_cwd $fish_color_cwd
-        end
-        set suffix '#'
-    case '*'
-        set color_cwd $fish_color_cwd
-        set suffix '>'
+            set suffix '>'
     end
 
     set -g __fish_git_prompt_show_informative_status 1
-    set -g __fish_git_prompt_color_branch magenta '--bold'
-    set -g __fish_git_prompt_color_dirtystate blue
-    set -g __fish_git_prompt_color_stagedstate yellow
-    set -g __fish_git_prompt_color_invalidstate red
-    set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-    set -g __fish_git_prompt_color_cleanstate green
+    set -g __fish_git_prompt_color_branch magenta
+    set -g __fish_git_prompt_color_dirtystate 06f
+    set -g __fish_git_prompt_char_dirtystate "+"
+    #set -g __fish_git_prompt_color_stagedstate yellow
+    #set -g __fish_git_prompt_color_invalidstate red
+    #set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    #set -g __fish_git_prompt_color_cleanstate green
 
-    echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt) "$suffix "
+    echo -en -s "$USER" @ "$__fish_prompt_hostname" ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt) "\n$suffix "
 end
 
 #set -g __fish_git_prompt_show_informative_status 1
